67dc31ed526c30945cd2b19790a3d6bf22258f09
CralpCode <clp64413@gmail.com>
2022-08-08 14:01:18 -0600

    Primer commit

---
+++ Valores.js
@@ -0,0 +1,11 @@
+40
+"Diego de Granada"
+
+true
+false
+
+null 
+undefined 
+
+[1,2,3]
+{nombre:"Diego"}
---
+++ Variables.js
@@ -0,0 +1,19 @@
+var nombre = "Carlos";
+
+ //declarar
+
+var edad;
+edad
+ //inicializar
+
+eddad = 30;
+edada
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre:"Carlos",
+    edad:30
+}
+
+persona;
